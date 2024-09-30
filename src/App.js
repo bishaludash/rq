5e40f6033753c6d039e00a0b233d1a605567c627
bishaludash <supertramp.bishal@gmail.com>
2024-09-30 17:58:38 +0200

    exploring queries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from "react";
 import "./index.css";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 const App = () => {
-  return <div>App</div>;
+  const getTodos = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts").then(
+      (response) => response.json(),
+    );
+    console.log(res);
+    return res;
+  };
+
+  /* Queries : gettting data from api [GET]
+    - automatic retries if fails
+  */
+  const posts = useQuery({ queryKey: ["todos"], queryFn: getTodos });
+
+  // mutation : changing some time of data [POST]
+  if (posts.isLoading) {
+    return <div>loading....</div>;
+  }
+
+  if (posts.isError) {
+    return <div>{JSON.stringify(posts.error)}</div>;
+  }
+
+  return (
+    <div>
+      {posts.data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
